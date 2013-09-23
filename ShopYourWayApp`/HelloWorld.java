7e4d053812078a36afa1a377d261b2f4bc7360c9
roni-itai <Roni@Ronis-MacBook-Air.local>
2013-09-24 00:32:24 +0300

    Testing 123

--- ShopYourWayApp`/HelloWorld.java
+++ ShopYourWayApp`/HelloWorld.java
@@ -2,6 +2,8 @@ public class HelloWorld {
 	public static void main(String [] args){
 		System.out.println("HelloWorld!");
 		System.out.println("Make a change!");
+		String x = "Itai Newbie!";
+		System.out.println(x);
 	}
 }
 
