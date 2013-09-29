546042aa55fb56eebbdb10c4380d4c3d7c7a6776
Itai Birenshtok <itai.biren@gmail.com>
2013-09-29 22:51:43 +0300

    No change

--- ShopYourWayApp`/HelloWorld.java
+++ ShopYourWayApp`/HelloWorld.java
@@ -1,10 +1,9 @@
 public class HelloWorld {
-	public static void main(String [] args){
+	public static void main(String[] args) {
 		System.out.println("HelloWorld!");
-		System.out.println("Make a change!");
-		String x = "ITAI IS THE TRUE Newbie!";
-		System.out.println(x);
-		System.out.println("זה כללי אני אמור לך");
+ 		System.out.println("Make a change!");
+ 		String x = "ITAI IS THE TRUE Newbie!";
+ 		System.out.println(x);
+		System.out.println("זה כללי אני אומר לך");
 	}
 }
-
