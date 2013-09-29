8f518137b7c417058a1b0c353b692c3253b6651d
Itai Birenshtok <itai.biren@gmail.com>
2013-09-30 01:46:06 +0300

    Add the UserInfo class
    We now have the ability to return the user likes

---
+++ ShopYourWayApp`/User/TestUserInfo.java
@@ -0,0 +1,14 @@
+package User;
+/**
+ * This Class is for testing the UserInfo.getLikesAsJson method.
+ * The Token is mine
+ * @author Itai Birenshtok
+ *
+ */
+public class TestUserInfo {
+
+	public static void main(String[] args) {
+		UserInfo user = new UserInfo("CAACEdEose0cBAChXmIHdw0BQZBfzvLLPb6KF8bZAOQYKVkNyrc7lYQLmWUi3DKt707VxMo1K5a2T3FmUPpBpqbNDwrhCKZBugDAkG6bSfUHTaMZCob2DB6g5NZCVRukGFDiRy08pgxRuV6EiJYfLjbZBZBEHgr6EfwdCIZCSWIqcXb5TPqhPqnZCZAjwkztiXBq5qVKnHmZAej5lgZDZD");
+		System.out.println(user.getLikesAsJson("likes"));
+	}
+}
---
+++ ShopYourWayApp`/User/UserInfo.java
@@ -0,0 +1,43 @@
+package User;
+import com.restfb.FacebookClient;
+import com.restfb.json.JsonArray;
+import com.restfb.json.JsonObject;
+import com.restfb.types.User;
+import com.restfb.DefaultFacebookClient;
+
+
+/**
+ * This class holds the UserInfo
+ * @author Itai Birenshtok
+ *
+ */
+public class UserInfo {
+	
+	/**
+	 * Class fields
+	 */
+	private String UserID;
+	private FacebookClient facebookClient;
+	
+	/**
+	 * Constructor
+	 * @param MY_ACCESS_TOKEN
+	 */
+	public UserInfo (String MY_ACCESS_TOKEN) {
+		FacebookClient facebookClient = new DefaultFacebookClient(MY_ACCESS_TOKEN);
+		this.facebookClient = facebookClient;
+		User user = facebookClient.fetchObject("me", User.class);
+		this.UserID = user.getId();
+	}
+	
+	/**
+	 * This method returns all the pages that the user likes
+	 * @param UrlRelativePath
+	 * @return The likes as a JsonArray
+	 */
+	public JsonArray getLikesAsJson (String UrlRelativePath) {
+		JsonObject photosConnection = facebookClient.fetchObject(this.UserID + "/" + UrlRelativePath, JsonObject.class);
+		JsonArray likesArray = photosConnection.getJsonArray("data");
+		return likesArray;
+	}
+}
