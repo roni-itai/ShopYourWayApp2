d11e902acc5424dfa1ec35222a7311fe75cb5b34
roni-itai <itai_biren@walla.co.il>
2013-10-01 12:10:36 +0300

    Arrange few things

--- ShopYourWayApp`/HelloWorld.java
+++ ShopYourWayApp`/HelloWorld.java
@@ -1,17 +1,9 @@
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("HelloWorld!");
-<<<<<<< HEAD
 		System.out.println("Make a change!");
-		String x = "ITAI IS THE TRUE Newbie!";
+		String x = "RONI IS THE TRUE Newbie!";
 		System.out.println(x);
 		System.out.println("זה כללי אני אמור לך");
-
-=======
- 		System.out.println("Make a change!");
- 		String x = "ITAI IS THE TRUE Newbie!";
- 		System.out.println(x);
-		System.out.println("זה כללי אני אומר לך");
->>>>>>> 0fca0ef642837b8997726f2ff7a20f0cc34a4851
 	}
 }
--- ShopYourWayApp`/User/TestUserInfo.java
+++ ShopYourWayApp`/User/TestUserInfo.java
@@ -8,7 +8,7 @@ package User;
 public class TestUserInfo {
 
 	public static void main(String[] args) {
-		UserInfo user = new UserInfo("CAACEdEose0cBAChXmIHdw0BQZBfzvLLPb6KF8bZAOQYKVkNyrc7lYQLmWUi3DKt707VxMo1K5a2T3FmUPpBpqbNDwrhCKZBugDAkG6bSfUHTaMZCob2DB6g5NZCVRukGFDiRy08pgxRuV6EiJYfLjbZBZBEHgr6EfwdCIZCSWIqcXb5TPqhPqnZCZAjwkztiXBq5qVKnHmZAej5lgZDZD");
+		UserInfo user = new UserInfo("CAACEdEose0cBAHtF88mfbztfUbeUFqJGZA8pKTPC3GSI4tu1UEDnrmxMp6DUsHCn9CZCuePJZCniJgYwvwzcfsU3P4pSfUWsAsN2s6Ir7WJtqi4eE9STcCQeFnSknhZAqHC7v3iTPwMZASZAQDXV5MxshfWqaZC2DEbtjeiNfLBqHXKYqNGVdmyfMDWCwuMqS0ZD");
 		System.out.println(user.getLikesAsJson());
 	}
 }
