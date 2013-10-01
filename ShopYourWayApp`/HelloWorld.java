aacb3a2f600505f4be23533b44090040438c9035
roni-itai <Roni@Ronis-MacBook-Air.local>
2013-10-01 19:44:38 +0300

    Hope thuis doesnt mess anything up

--- ShopYourWayApp`/HelloWorld.java
+++ ShopYourWayApp`/HelloWorld.java
@@ -2,10 +2,13 @@ public class HelloWorld {
 	public static void main(String [] args){
 		System.out.println("HelloWorld!");
 		System.out.println("Make a change!");
-		String x = "ITAI IS THE TRUE Newbie!";
+		String x = "RONI IS THE TRUE Newbie!";
 		System.out.println(x);
 		System.out.println("זה כללי אני אמור לך");
+<<<<<<< HEAD
 
+=======
+>>>>>>> d11e902acc5424dfa1ec35222a7311fe75cb5b34
 	}
 }
 
